079c25ae1027771fedbc662c7ff4a83e4b33dd0c
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-13 14:26:01 +0200

    Correggi la formattazione delle stringhe e aggiungi la stampa dei nomi in console

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,10 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names.join(", "));
+
+names.forEach((nome) => {
+  console.log(nome);
+});
